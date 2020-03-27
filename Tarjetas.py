32266e73c5d66306b2332bd86a4f1e647253384e
Eldelosgatos <45078669+Eldelosgatos@users.noreply.github.com>
2020-03-27 04:24:09 -0500

    Update Tarjetas.py

--- Tarjetas.py
+++ Tarjetas.py
@@ -1,7 +1,22 @@
-def Jugador(nombre):
-    mano = [];
 
-def Carta():
+class Judador: 
+    def __init__(jugador):
+        jugador.nombre = jugador;
+        jugador.mano = [];
+    def despliega_mano():
+        
 
-    
-def Baraja():
+class Carta:
+    def __init__(jugador):
+        jugador.valor=;
+        jugador.figura=;
+    def __str__():
+        carta=jugador.valor+jugador.figura;
+        
+class Baraja:
+    def __init__():
+        diccionario= {'A':20, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13}
+        lista_cartas= ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        lista_figuras= ['P', 'T', 'C', 'D']
+    def genera_mano():
+    def guarda_jugador():
