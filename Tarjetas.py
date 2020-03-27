5463e5bf643cca2bc163e3902d7d84e4a35327ee
Eldelosgatos <45078669+Eldelosgatos@users.noreply.github.com>
2020-03-27 05:40:16 -0500

    Update Tarjetas.py

--- Tarjetas.py
+++ Tarjetas.py
@@ -2,7 +2,7 @@
 class Judador: 
     def __init__(jugador):
         jugador.nombre = jugador;
-        jugador.mano = [];
+        jugador.mano = mano;
     def despliega_mano():
         
 
@@ -11,12 +11,23 @@ class Carta:
         jugador.valor=;
         jugador.figura=;
     def __str__():
-        carta=jugador.valor+jugador.figura;
+        jugador.carta=jugador.valor+jugador.figura;
         
 class Baraja:
-    def __init__():
+    def __init__(jugador):
         diccionario= {'A':20, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13}
-        lista_cartas= ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        lista_cartas= ['AP', '2P', '3P', '4P', '5P', '6P', '7P', '8P', '9P', '10P', 'JP', 'QP', 'KP',
+                       'AT', '2T', '3T', '4T', '5T', '6T', '7T', '8T', '9T', '10T', 'JT', 'QT', 'KT',
+                       'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC',
+                       'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD']
         lista_figuras= ['P', 'T', 'C', 'D']
-    def genera_mano():
+        lista_jugadores= []
+        lista_jugadores.append(jugador);
+    def genera_mano(manos):
+        mano=[];
+        while len(mano) != manos:
+            uno= randint(0,51)
+            if lista_cartas[uno] not in mano:
+                mano.append(lista_cartas[uno])
+        
     def guarda_jugador():
