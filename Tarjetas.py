3d1fa9f95b7887a63de4a3ad72669cc3ae0675ea
Eldelosgatos <45078669+Eldelosgatos@users.noreply.github.com>
2020-03-28 00:17:28 -0500

    Update Tarjetas.py

--- Tarjetas.py
+++ Tarjetas.py
@@ -1,33 +1,58 @@
-
+import random
 class Judador: 
-    def __init__(jugador):
-        jugador.nombre = jugador;
-        jugador.mano = mano;
-    def despliega_mano():
+    
+    jugador = None
+    mano = None
+    puntos = None
+    
+    def __init__(self, jugador, baraja):
+        self.nombre = jugador
+        self.mano = mano
+        self.puntos= 0
+        baraja.guarda_jugador(self)
         
+    def despliega_mano(self, baraja):
+        mano_desplegada = []
+        print(self.nombre)
+        for c in self.mano:
+            cartas = f"{c.display(baraja.diccionario_cartas)}"
+            print(cartas)
+            mano_desplegada.append(cartas)
+        return mano_desplegada
 
 class Carta:
-    def __init__(jugador):
-        jugador.valor=;
-        jugador.figura=;
-    def __str__():
-        jugador.carta=jugador.valor+jugador.figura;
+    
+    valor= None
+    figura = None
+    
+    def __init__(self):
+        self.valor= valor
+        self.figura= figura
         
+    def __str__(self, diccionario):
+        carta_uno = diccionario[self.valor]
+        return f"{carta_uno}-{self.figura}"
+    
 class Baraja:
-    def __init__(jugador):
-        diccionario= {'A':20, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13}
+    diccionario = None
+    lista_figuras = None
+    lista_jugadores= None
+    lista_cartas= None
+    
+    def __init__(self, lista_cartas ):
+        self.diccionario= {'A':20, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13}
         lista_cartas= ['AP', '2P', '3P', '4P', '5P', '6P', '7P', '8P', '9P', '10P', 'JP', 'QP', 'KP',
                        'AT', '2T', '3T', '4T', '5T', '6T', '7T', '8T', '9T', '10T', 'JT', 'QT', 'KT',
                        'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC',
                        'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD']
-        lista_figuras= ['P', 'T', 'C', 'D']
-        lista_jugadores= []
-        lista_jugadores.append(jugador);
+        self.lista_figuras= ['P', 'T', 'C', 'D']
+        self.lista_jugadores= []
     def genera_mano(manos):
-        mano=[];
-        while len(mano) != manos:
-            uno= randint(0,51)
-            if lista_cartas[uno] not in mano:
-                mano.append(lista_cartas[uno])
+        for x in self.lista_jugadores:
+            x.mano = random.sample(self.lista_cartas, num_cartas)
+
+            for y in x.mano:
+                self.lista_cartas.remove(y)
         
-    def guarda_jugador():
+    def guarda_jugador(self, jugador):
+        self.lista_jugadores.append(jugador)
