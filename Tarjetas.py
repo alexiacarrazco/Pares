6953daceae6e34d32ea7b7a07099adedb591d80d
Eldelosgatos <45078669+Eldelosgatos@users.noreply.github.com>
2020-03-28 00:47:55 -0500

    Update Tarjetas.py

--- Tarjetas.py
+++ Tarjetas.py
@@ -1,58 +1,58 @@
 import random
-class Judador: 
-    
-    jugador = None
-    mano = None
-    puntos = None
-    
-    def __init__(self, jugador, baraja):
-        self.nombre = jugador
+
+def baraj(figuras, diccionario):
+    lista_cartas = []
+    for figura in figuras:
+        for carta, valor in diccionario_cartas.items():
+            naipe = carta + " " + figura
+            carta = Carta(valor,naipe)
+            lista_cartas.append(carta)
+    return lista_cartas
+
+class Jugador: 
+    def _init_(self, nombre, mano):
+        self.nombre = nombre
         self.mano = mano
-        self.puntos= 0
-        baraja.guarda_jugador(self)
-        
-    def despliega_mano(self, baraja):
-        mano_desplegada = []
+    def despliega_mano(self):
         print(self.nombre)
-        for c in self.mano:
-            cartas = f"{c.display(baraja.diccionario_cartas)}"
-            print(cartas)
-            mano_desplegada.append(cartas)
-        return mano_desplegada
+        for x in self.mano:
+            print(x)
+        print("*****")
+        
 
 class Carta:
-    
-    valor= None
-    figura = None
-    
-    def __init__(self):
-        self.valor= valor
-        self.figura= figura
+    def _init_(self, valor, figura):
+        self.valor=valor
+        self.figura=figura
+    def _str_(jugador):
+        self.carta=self.valor+self.figura;
+        return self.carta
         
-    def __str__(self, diccionario):
-        carta_uno = diccionario[self.valor]
-        return f"{carta_uno}-{self.figura}"
-    
+
 class Baraja:
-    diccionario = None
-    lista_figuras = None
-    lista_jugadores= None
-    lista_cartas= None
-    
-    def __init__(self, lista_cartas ):
+    def _init_(self):
         self.diccionario= {'A':20, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13}
-        lista_cartas= ['AP', '2P', '3P', '4P', '5P', '6P', '7P', '8P', '9P', '10P', 'JP', 'QP', 'KP',
+        self.lista_cartas= ['AP', '2P', '3P', '4P', '5P', '6P', '7P', '8P', '9P', '10P', 'JP', 'QP', 'KP',
                        'AT', '2T', '3T', '4T', '5T', '6T', '7T', '8T', '9T', '10T', 'JT', 'QT', 'KT',
                        'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC',
                        'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD']
-        self.lista_figuras= ['P', 'T', 'C', 'D']
-        self.lista_jugadores= []
-    def genera_mano(manos):
-        for x in self.lista_jugadores:
-            x.mano = random.sample(self.lista_cartas, num_cartas)
-
-            for y in x.mano:
-                self.lista_cartas.remove(y)
+        self.lista_figuras= ['P', 'T', 'C', 'D'] 
+        lista_jugadores= []
+        self.lista_cartas = baraj(self.figuras, self.diccionario)
         
-    def guarda_jugador(self, jugador):
-        self.lista_jugadores.append(jugador)
+    
+    def genera_mano(manos):
+        i = 0
+        mano= []
+        while i < 5:
+         if len(manos.lista_cartas)==0:
+                print("Se han acabado las cartas!")
+                break
+            r = random.choice(manos.lista_jugadores)
+            mano.append(r)
+            manos.lista_cartas.remove(r)
+            i += 1
+        return mano
+        
+    def guarda_jugador(manos,jugador):
+        manos.lista_jugadores.append(jugador);
